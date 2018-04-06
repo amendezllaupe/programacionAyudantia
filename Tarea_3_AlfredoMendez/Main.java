6484714a42b7270582e36c5e4e0e519b399107df
amendezllaupe <37369457+amendezllaupe@users.noreply.github.com>
2018-04-06 19:30:51 -0300

    Add files via upload
    Tarea 3 (Domingo 8 de Abril)

---
+++ Tarea_3_AlfredoMendez/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea_programación;
+
+/**
+ *
+ * @author hypoc
+ */
+public class Main {
+    public static void main(String[]args){
+        InventarioLuchadores inventario = new InventarioLuchadores();
+        InventarioObjetos invObj = new InventarioObjetos();
+        
+        
+        invObj.ejecutar();
+        //inventario.menu();
+    }
+}
