5ca4bd5a18d94a64e4b8a533fc1f3e5f28182412
amendezllaupe <37369457+amendezllaupe@users.noreply.github.com>
2018-07-23 01:09:58 -0400

    Update Observacion.java

--- TAREA FINAL/GUI/Observacion.java
+++ TAREA FINAL/GUI/Observacion.java
@@ -5,10 +5,42 @@
  */
 package GUI;
 
+import Desarrollo.Batalla;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 /**
  *
  * @author hypoc
  */
-public class Observacion extends JFrame implemets ActionListener{
+public class Observacion extends JFrame{
     
+    private TextArea showText;
+    
+    public Observacion(String title, Batalla batalla){
+        super(title);
+        
+        this.showText = new TextArea(batalla.getResultados());
+        
+        elementsSize();
+        
+        addWindow();
+        
+        this.showText.setEditable(false);      
+        
+    }
+    
+    private void addWindow(){
+        this.add(this.showText);
+        this.add(new JLabel());
+    }
+    
+    private void elementsSize(){
+        this.setSize(600,600);
+        
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        this.setLocation(dim.width/2-this.getSize().width/2, dim.height/2-this.getSize().height/2);
+        
+        this.showText.setBounds(10,10,490,490);
+    }
 }
